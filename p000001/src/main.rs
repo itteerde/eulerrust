0eee143653e23687fa9e8570eaf20bb83180ad9f
Erik Itter <erik.itter@gmail.com>
2024-08-27 21:48:25 +0200

    Problem 1

--- p000001/src/main.rs
+++ p000001/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Problem 1");
+
+    let mut result : i64 = 0;
+    for n in 1..999 {
+        if n % 3 == 0 || n % 5 == 0 {
+            result += n;
+        }
+    }
+    println!("result: {}", result);
 }
